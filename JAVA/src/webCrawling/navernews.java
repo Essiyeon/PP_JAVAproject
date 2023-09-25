2c79f40187b5ea82463a07605aa353d69f983b34
siyeon <aeiou0302@icloud.com>
2023-09-25 18:48:13 +0900

    2023-09-25 Second Commit

--- JAVA/src/webCrawling/navernews.java
+++ JAVA/src/webCrawling/navernews.java
@@ -1,3 +1,13 @@
+/*
+ * 첫 번째 자바프로젝트
+ * 주제 : javafx, jsoup 를 이용한 네이버 뉴스웹 크롤링 GUI
+ * 주제 선정 배경 : 대부분의 뉴스사이트는 무분별한 광고가 너무 많이 노출됨, 헤드라인만 읽고 싶음
+ * 추가하고 싶은 기능 (optional) : 사진도 같이 뜨게하고싶음, 새로고침버튼, 주제별(정치,과학 등)모아보기 버튼
+ * 
+ * 2023-09-25 Second Commit
+ * 네이버 뉴스 IT/과학카테고리의 IT일반 탭의 뉴스를 크롤링합니다.
+ * 뉴스의 헤드라인만 GUI창에 출력합니다.
+ */
 package webCrawling;
 
 import javafx.application.Application;
